6d4b1f376b02f99abbd22c77652bf925dd1ca575
shivamdan <shivamdan24052003@gmail.com>
2024-11-01 15:06:22 +0530

    Sorted the predicted probabilities of all classes in descending order, for an INPUT

--- M9 - Star Type API/api.py
+++ M9 - Star Type API/api.py
@@ -13,12 +13,21 @@ def index_route():
     return {"Health":"Ok"}
 
 @app.post('/predict', response_model=StarTypePrediction)
-# def prediction(temperature, luminosity, radius, abs_mag):
 def prediction(sp: StarProperties):
+# def prediction(temperature, luminosity, radius, abs_mag):
     input_features = [[sp.temperature, sp.luminosity, sp.radius, sp.abs_mag]]
     predicted_class, probabs, classes = make_predictions(model, input_features)
+    pred_probs = dict(zip(classes, probabs))
+    sorted_pred_probs = dict(sorted(pred_probs.items(), key = lambda item:item[1], reverse=True))
     return {
-        'predicted_probabilities' : dict(zip(classes, probabs)),
+        'predicted_probabilities' : sorted_pred_probs,
         'predicted_class' : predicted_class,
         'confidence_score' : str(round(np.max(probabs),3)*100) + '%' # Returning the maximum value among all the probabilities
     }
+
+'''
+Test on real star data taken from Wikipedia
+Betelguese (Supergiant) : https://en.wikipedia.org/wiki/Betelgeuse
+Beta Pictoris (Main Sequence) : https://en.wikipedia.org/wiki/Beta_Pictoris
+Sirius (White Dwarf) : https://en.wikipedia.org/wiki/Sirius
+'''
